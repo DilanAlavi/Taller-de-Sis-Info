4b3a09e8c8d049bca6c8e217c7fea4e888e1fcf4
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-10-03 18:11:48 -0400

    Icono de usuario

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { FaUser } from 'react-icons/fa'; 
 const Header = () => {
   return (
     <header className="header">
@@ -14,9 +14,13 @@ const Header = () => {
             <li className="navbar-item"><Link to="/login">Login</Link></li>
             <li className="navbar-item"><Link to="/register">Register</Link></li>
             <li className="navbar-item"><Link to="/perritoperdidoform">Vi un Perrito Perdido</Link></li>
-            <li className="navbar-item"><Link to="/user">User</Link></li>
             <li className="navbar-item"><Link to="/ia">Clasificador IA</Link></li>
             <li className="navbar-item"><Link to="/dog-recognition">Reconocimiento de Razas</Link></li>
+            <li className="navbar-item">
+              <Link to="/user" onClick={() => console.log("Navegando a la página de usuario")}>
+                <FaUser size={24} />
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
