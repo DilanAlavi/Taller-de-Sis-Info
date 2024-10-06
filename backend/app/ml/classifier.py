110848ebd0831542d99dd955cb8d50afdcd26f23
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2024-10-06 00:24:21 -0400

    Se ha mejorado el entrenamiento y ademas se ha eliminado archivos .pyc y __pycache__  y actualizar .gitignore

--- backend/app/ml/classifier.py
+++ backend/app/ml/classifier.py
@@ -8,8 +8,7 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-# Ruta al modelo entrenado
-MODEL_PATH = os.path.join(BASE_DIR, '..', 'Entrenamiento', 'model_training', 'modelo_perrito_entrenado.h5')
+MODEL_PATH = os.path.join(BASE_DIR, '..', 'Entrenamiento', 'model_training', 'modelo_perro_entrenado.h5')
 
 
 
@@ -20,35 +19,33 @@ def cargar_o_crear_modelo():
         model = tf.keras.models.load_model(MODEL_PATH)
     else:
         print("Creando un nuevo modelo...")
-        print(f"hola probando")
         entrenar_modelo()  
-        model = tf.keras.models.load_model(MODEL_PATH)  # Cargar el modelo recién entrenado
+        model = tf.keras.models.load_model(MODEL_PATH)  
     return model
 
-def update_model_with_new_image(new_image_path, label):
-    model = cargar_o_crear_modelo()
-
-    # Preprocesar la nueva imagen
-    new_image = preprocess_image(new_image_path)
+# def update_model_with_new_image(new_image_path, label):
+#     model = cargar_o_crear_modelo()
+#     # Preprocesar la nueva imagen
+#     new_image = preprocess_image(new_image_path)
     
-    # Ajuste pequeño del modelo
-    model.fit(new_image, np.array([label]), epochs=1, verbose=0)
+#     # Ajuste pequeño del modelo
+#     model.fit(new_image, np.array([label]), epochs=1, verbose=0)
 
-    # Guardar el modelo actualizado
-    model.save(MODEL_PATH)
-    print(f"Modelo actualizado y guardado en {MODEL_PATH}")
+#     # Guardar el modelo actualizado
+#     model.save(MODEL_PATH)
+#     print(f"Modelo actualizado y guardado en {MODEL_PATH}")
 
 def classify_pet(image_path):
     model = cargar_o_crear_modelo()
     processed_image = preprocess_image(image_path, add_batch_dim=True)
     prediction = model.predict(processed_image)
-    print(model)
-    if prediction[0][0] > 0.6:
+    print(prediction[0][0])
+    if prediction[0][0] > 0.5:
         return "Perro"
     else:
-        return "Gato"
+        return "No perro"
 
-# Para la API
+
 def classify_pet_api(image_path):
     try:
         result = classify_pet(image_path)
--- backend/app/ml/tensor_perritos.py
+++ backend/app/ml/tensor_perritos.py
@@ -1,40 +1,40 @@
 import tensorflow as tf
+from tensorflow import keras
 import os
 from PIL import Image
 import numpy as np
 
 BASE_DIR = os.path.dirname(__file__)
 
-# Ruta al modelo entrenado
-MODEL_PATH = os.path.join(BASE_DIR, '..', 'Entrenamiento', 'model_training', 'modelo_perrito_entrenado.h5')
+MODEL_PATH = os.path.join(BASE_DIR, '..', 'Entrenamiento', 'model_training', 'modelo_perro_entrenado.h5')
 
 
-def cargar_o_crear_modelo(X_train = "", y_train = ""):
+def cargar_o_crear_modelo(X_train="", y_train=""):
     if os.path.exists(MODEL_PATH):
         print("Cargando el modelo preentrenado...")
         model = tf.keras.models.load_model(MODEL_PATH)
     else:
         print("Creando un nuevo modelo...")
-        model = tf.keras.Sequential([
-            tf. keras.layers.InputLayer(shape=(224, 224, 3)),
-            tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(1, activation='sigmoid') 
-
+        model = keras.Sequential([
+            keras.layers.InputLayer(input_shape=(224, 224, 3)),
+            keras.layers.Conv2D(32, (3, 3), activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            keras.layers.Conv2D(128, (3, 3), activation='relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            keras.layers.Flatten(),
+            keras.layers.Dense(128, activation='relu'),
+            keras.layers.Dense(1, activation='sigmoid') 
         ])
-        # Optimización con SGD
-        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
-                      loss='binary_crossentropy',
+        
+        model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.0001),
+                      loss='binary_crossentropy',  
                       metrics=['accuracy'])
-        model.fit(X_train, y_train, epochs=5)  # Aquí debes proporcionar X_train e y_train
 
-        # Guardar el modelo entrenado
+
+        model.fit(X_train, y_train, epochs=13)  
+        print(model.history)
         model.save(MODEL_PATH)
         print(f"Modelo guardado en {MODEL_PATH}")
 
@@ -47,9 +47,10 @@ def preprocess_image(image_path, add_batch_dim=False):
     except Exception as e:
         raise ValueError(f"No se pudo abrir la imagen: {e}")
 
-    img = img.resize((224, 224))  # Redimensionar la imagen
-    img_array = np.array(img) / 255.0  # Normalizar
+    img = img.resize((224, 224))  
+    img_array = np.array(img)  
 
     if add_batch_dim:
-        return np.expand_dims(img_array, axis=0)  # Añadir una dimensión extra para el batch
-    return img_array  # Devolver sin la dimensión del batch
+        img_array = img_array / 255.0
+        return np.expand_dims(img_array, axis=0)  
+    return img_array  
