4e7b4cd21e8576fb6f1da2e1bac178f997ee8efd
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2024-11-02 15:18:57 -0400

    se mejoro el entreno de reconocimiento de si es o no un perrito

--- backend/app/ml/classifier.py
+++ backend/app/ml/classifier.py
@@ -2,29 +2,23 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import os
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
 
 BASE_DIR = os.path.dirname(__file__)
 print(BASE_DIR)
 
-# MODEL_PATH = os.path.join(BASE_DIR, '..', 'Entrenamiento', 'model_training', 'modelo_perro_entrenado.h5')
+MODEL_PATH = os.path.join(BASE_DIR, 'TallerPerritos.keras')
 
-MODEL_PATH = "E:\\Univerisda\\2024-2\\Gestion de Calidad\\finalModel2.h5"
+#MODEL_PATH = "C:\\Users\\Sebas\\Documents\\TALLER SIS INFO\\TallerPerritos.keras"
 
 
-def preprocess_image(image_path, add_batch_dim=False):
-    try:
-        img = Image.open(image_path).convert('RGB')
-    except Exception as e:
-        raise ValueError(f"No se pudo abrir la imagen: {e}")
-
+def load_and_preprocess_image(image_path):
+    img = Image.open(image_path).convert('RGB')  
     img = img.resize((224, 224))  
     img_array = np.array(img)  
-
-    if add_batch_dim:
-        img_array = img_array / 255.0
-        return np.expand_dims(img_array, axis=0)  
-    return img_array 
+    img_array = preprocess_input(img_array)  
+    return np.expand_dims(img_array, axis=0) 
 
 
 def cargar_o_crear_modelo():
@@ -35,23 +29,10 @@ def cargar_o_crear_modelo():
         print("Error")
     return model
 
-# def update_model_with_new_image(new_image_path, label):
-#     model = cargar_o_crear_modelo()
-#     # Preprocesar la nueva imagen
-#     new_image = preprocess_image(new_image_path)
-    
-#     # Ajuste pequeño del modelo
-#     model.fit(new_image, np.array([label]), epochs=1, verbose=0)
-
-#     # Guardar el modelo actualizado
-#     model.save(MODEL_PATH)
-#     print(f"Modelo actualizado y guardado en {MODEL_PATH}")
-
-
 
 def classify_pet(image_path):
     model = cargar_o_crear_modelo()
-    processed_image = preprocess_image(image_path, add_batch_dim=True)
+    processed_image = load_and_preprocess_image(image_path)
     prediction = model.predict(processed_image)
     print(prediction[0][0])
     if prediction[0][0] > 0.5:
