274bc2c0eb00a8b8509217a92bf6a0dc5310d5cc
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2024-11-20 11:47:40 -0400

    Pagina de registro muestra datos del back

--- frontend/src/components/ReportList/ReportList.js
+++ frontend/src/components/ReportList/ReportList.js
@@ -1,52 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ReportList.css';
+import axios from 'axios';
 
 const ReportList = () => {
-  // Datos hardcodeados 
-  const reports = [
-    {
-      id: 1,
-      userName: 'Juan Pérez',
-      reason: 'Publicación de contenido inapropiado',
-      date: '2024-11-01',
-    },
-    {
-      id: 2,
-      userName: 'María López',
-      reason: 'Comportamiento ofensivo',
-      date: '2024-11-10',
-    },
-    {
-      id: 3,
-      userName: 'Carlos García',
-      reason: 'Uso de lenguaje inapropiado',
-      date: '2024-11-15',
-    },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [reportes, setReportes] = useState(null);
+  // const [perrosPerdidos, setPerrosPerdidos] = useState([]);
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+      const data = async () => {
+        try {
+          const response = await axios.get('http://127.0.0.1:8000/reporte/');
+          setReportes(response.data);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+      data();
+    }, []);
 
   return (
-    <div className="report-list-container">
-      <h1>Lista de Reportes de Usuarios</h1>
-      <table className="report-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Usuario</th>
-            <th>Motivo</th>
-            <th>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((report) => (
-            <tr key={report.id}>
-              <td>{report.id}</td>
-              <td>{report.userName}</td>
-              <td>{report.reason}</td>
-              <td>{report.date}</td>
+    <div>
+    {(loading && !reportes) ? (
+      <p>Cargando reportes</p>
+    ) : (
+      <div className="report-list-container">
+        <h1>Lista de Reportes de Usuarios</h1>
+        <table className="report-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Usuario</th>
+              <th>Motivo</th>
+              <th style={{textAlign:"left"}}>Descripcion</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {reportes.map((report) => (
+              <tr key={report.id}>
+                <td>{report.id}</td>
+                <td>{report.usuario.nombre}</td>
+                <td>{report.motivo}</td>
+                <td style={{textAlign:"left"}}>{report.descripcion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )}
     </div>
   );
 };
