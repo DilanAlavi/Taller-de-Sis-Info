c105f8058c8f774efdf19eb3fbf3173b4d71f32a
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2024-11-28 17:06:50 -0400

    protect routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,26 +29,23 @@ const App = () => {
         <Header />
         <main className="App-main">
           <Routes>
+            <Route path="/perritoperdidoform" element={<ProtectedRoute element={<PerritoPerdidoForm />} />} />
+            <Route path="/dog-recognition" element={<ProtectedRoute element={<DogRecognition />} />} />
+            <Route path="/user" element={<ProtectedRoute element={<User />} />} />
+            <Route path='/perritovistoform' element={<ProtectedRoute element={<PerritoVistoForm />} />} />
+            <Route path="/report/:id" element={<ProtectedRoute element={<ReportUser />} />} />
+            <Route path="/report-list" element={<ProtectedRoute element={<ReportList />} />} />
+
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            
-            {/* Ruta protegida */}
-            <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
-            
-            <Route path="/perritoperdidoform" element={<PerritoPerdidoForm />} />
-            <Route path="/dog-recognition" element={<DogRecognition />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/perfil-user/:id" element={<PerfilUser />} />
-            {/* Ruta protegida */}
-            <Route path="/user" element={<ProtectedRoute element={<User />} />} />
             <Route path="/contacto" element={<Contacto />} />
             <Route path="/infoPerros" element={<InfoPerros />} />
-            <Route path='/perritovistoform' element={<PerritoVistoForm />} />
             <Route path='/paginaperroperdido' element={<PaginaPerrosPerdidos />} />
             <Route path='/paginaperrovisto' element={<PaginaPerrosVistos />} />
             <Route path="/perfil-perro/:id" element={<PerfilPerro />} />
-            <Route path="/report/:id" element={<ReportUser />} />
-            <Route path="/editar_perro/:id" element={<EditPerro />} />
-            <Route path="/report-list" element={<ReportList />} />
+            <Route path="/editar_perro/:id" element={<ProtectedRoute element={<EditPerro />} />} />
             <Route path="/recaudacionFondos" element={<RecaudacionFondos />} />
           </Routes>
         </main>
--- frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../../AuthContext';
+
+const token_data = async (logout) => { 
+  try {
+    await axios.get('http://localhost:8000/auth/profile');
+  } catch (error) {
+    console.log("Token vencido");
+    logout();
+  }
+}
 
 
 const ProtectedRoute = ({ element }) => {
+  const {logout} = useContext(AuthContext);
   const token = localStorage.getItem('access_token');
   console.log("esto es el token:", token);
+  token_data(logout);
+  
+
 
   if (!token) {
     return <Navigate to="/login" replace />;
