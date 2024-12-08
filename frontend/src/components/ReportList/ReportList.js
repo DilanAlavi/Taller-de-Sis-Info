9b7cbaef92c06f689b534ff2b9d67960157831d6
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-12-07 23:58:58 -0400

    alinear

--- frontend/src/components/ReportList/ReportList.js
+++ frontend/src/components/ReportList/ReportList.js
@@ -51,7 +51,7 @@ const ReportList = () => {
                     <td data-label="ID">{report.id}</td>
                     <td data-label="Usuario">{report.usuario.nombre}</td>
                     <td data-label="Motivo">{report.motivo}</td>
-                    <td data-label="Descripción" style={{ textAlign: 'left' }}>{report.descripcion}</td>
+                    <td data-label="Descripción">{report.descripcion}</td>
                   </tr>
                 ))}
               </tbody>
