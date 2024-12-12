3cb6e4ec882404d27801d5809f35a6380851824b
AlessanderMorales <alessmoralesmo@gmail.com>
2024-12-12 18:34:49 -0400

    Refactorizando pagina no encontrada y Login

--- frontend/src/paginaNoEncontrada/PaginaNoEncontrada.js
+++ frontend/src/paginaNoEncontrada/PaginaNoEncontrada.js
@@ -1,23 +1,28 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './PaginaNoEncontrada.css';
-
 const PaginaNoEncontrada = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       navigate('/home');
-    }, 4000); 
+    }, 40000); 
     return () => clearTimeout(timeoutId);
   }, [navigate]);
 
   return (
     <div className="not-found-page">
-      <h1 className="error-code">404</h1>
-      <h2 className="error-message-page">Página no encontrada</h2>
-      <p>Lo sentimos, la página que buscas no existe.</p>
-      <p>Serás redirigido al inicio en unos segundos...</p>
+      <div className="contenedor">
+        <img
+          src="/images/notfound.png"
+          alt="Lost Dog Icon"
+          className="error-icon"
+        />
+        <h1 className="error-code">404</h1>
+        <h2 className="error-message-page">Oops, no encontramos esta página</h2>
+        <p>La página que estás buscando no existe o fue movida.</p>
+      </div>
     </div>
   );
 };
