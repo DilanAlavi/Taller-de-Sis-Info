a5e460a532b40c4c1243cb461c39835e336e2d15
AlessanderMorales <alessmoralesmo@gmail.com>
2024-12-14 00:55:48 -0400

    areglando foter

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,15 +4,35 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className='footer-container'>
-        <div className="footer-links">
-          <Link to="/home" className="footer-link">Home</Link>
-          <Link to="/dog-recognition" className="footer-link">Reconocimiento de Razas</Link>
-          <Link to="/PerritoPerdidoForm" className="footer-link">Reporta un Perrito Perdido</Link>
-          <Link to="/PerritoVistoForm" className="footer-link">Reporta un Perrito Visto</Link>
-          <Link to="/contacto" className="footer-link">Contacto</Link>
-          <Link to="/infoPerros" className="footer-link">Información</Link>
-        </div>
+      <div className="footer-container">
+        <table className="footer-table">
+          <tbody>
+            <tr>
+              <td>
+                <Link to="/home" className="footer-link">Home</Link>
+              </td>
+              <td>
+                <Link to="/dog-recognition" className="footer-link">Reconocimiento de Razas</Link>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Link to="/PerritoPerdidoForm" className="footer-link">Reporta un Perrito Perdido</Link>
+              </td>
+              <td>
+                <Link to="/PerritoVistoForm" className="footer-link">Reporta un Perrito Visto</Link>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Link to="/contacto" className="footer-link">Contacto</Link>
+              </td>
+              <td>
+                <Link to="/infoPerros" className="footer-link">Información</Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <p>© 2024 Only Lost Pets. Todos los derechos reservados.</p>
       </div>
     </footer>
