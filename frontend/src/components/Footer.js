d28623d2f89fb8822260c475ee8ba545d879e71d
AlessanderMorales <alessmoralesmo@gmail.com>
2024-12-14 23:12:19 -0400

    Areglado el footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,35 +5,15 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <table className="footer-table">
-          <tbody>
-            <tr>
-              <td>
-                <Link to="/home" className="footer-link">Home</Link>
-              </td>
-              <td>
-                <Link to="/dog-recognition" className="footer-link">Reconocimiento de Razas</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Link to="/PerritoPerdidoForm" className="footer-link">Reporta un Perrito Perdido</Link>
-              </td>
-              <td>
-                <Link to="/PerritoVistoForm" className="footer-link">Reporta un Perrito Visto</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <Link to="/contacto" className="footer-link">Contacto</Link>
-              </td>
-              <td>
-                <Link to="/infoPerros" className="footer-link">Información</Link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <p>© 2024 Only Lost Pets. Todos los derechos reservados.</p>
+        <div className="footer-links">
+          <Link to="/home" className="footer-link">Home</Link>
+          <Link to="/dog-recognition" className="footer-link">Reconocimiento de Razas</Link>
+          <Link to="/PerritoPerdidoForm" className="footer-link">Reporta un Perrito Perdido</Link>
+          <Link to="/PerritoVistoForm" className="footer-link">Reporta un Perrito Visto</Link>
+          <Link to="/contacto" className="footer-link">Contacto</Link>
+          <Link to="/infoPerros" className="footer-link">Información</Link>
+        </div>
+        <p>© 2024 Only Lost Dogs. Todos los derechos reservados.</p>
       </div>
     </footer>
   );
